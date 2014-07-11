ab4f7ab37987dd497b72d98e467560af78ac6c39
phaggood <patrick.haggood@gmail.com>
2014-07-11 03:53:18 -0400

    finally a map that displays

--- js/controllers.js
+++ js/controllers.js
@@ -1,30 +1,47 @@
 angular.module('starter.controllers', [])
 
+
+
     .controller('DashCtrl', function($scope) {
     })
 
     .controller('MapCtrl', function($scope, $cordovaGeolocation) {
         $scope.msg = "";
+        $scope.coords = [0,0];
+        $scope.mapVisible = true;
+
         var init = function () {
+            var mapOptions = {};
+            var map = new google.maps.Map(document.getElementById("map"),
+                mapOptions);
+
+            $scope.map = map;
 
             // get coords
             $cordovaGeolocation.getCurrentPosition().then(function(position) {
                 // Position here: position.coords.latitude, position.coords.longitude
                 console.log("setting map");
-                $scope.msg = position.toString();
-                // http://angular-google-maps.org/use
-                $scope.map = {
-                    center: {
-                        latitude: parseFloat(position.coords.latitude),
-                        longitude: parseFloat(position.coords.longitude)
-                    },
-                    zoom: 8
-                }
-
+                $scope.msg = position.coords.latitude + ":" + position.coords.longitude;
+                $scope.updateCenter(parseFloat(position.coords.latitude), parseFloat(position.coords.longitude));
             }, function(err) {
-                // error
+                $scope.msg = "unable to determine location";
             });
         };
+
+
+        $scope.updateCenter = function(lat, lng) {
+            /*var mapOptions = {
+                center: new google.maps.LatLng(0,0),
+                zoom: 16,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };*/
+            $scope.map.setCenter(new google.maps.LatLng(lat, lng));
+            $scope.map.setZoom(16);
+            $scope.centerLat = lat;
+            $scope.centerLng = lng;
+            $scope.mapVisible =true;
+        };
+
 
         init();
     })
